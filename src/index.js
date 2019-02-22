f1b7eab84148dab7a7f018b3175ffa5b1b6cd75c
Richard Lynch <lynchardrich@gmail.com>
2019-02-22 17:48:14 +0000

    fixed autoplay chrome error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-//sort out
-//play pause function
-//dataarray only goes up to 620?
+import * as d3 from "d3";
 
 const ICON = {
 	play: "./img/play.svg",
@@ -9,7 +7,7 @@ const ICON = {
 
 const width = window.innerWidth,
 	height = window.innerHeight,
-	tau = 2*Math.PI;
+	tau = 2 * Math.PI;
 
 let audioCtx = new AudioContext();
 const audioEl = document.querySelector("audio");
@@ -21,8 +19,11 @@ analyser.fftSize = 8192;
 let dataArray = new Uint8Array(analyser.frequencyBinCount);
 let posArray = getPositions(dataArray.length);
 let timer;
-let color = d3.scaleLinear().domain([0, dataArray.length/4]).range([0,1]);
-let colorInt = d3.interpolateHclLong('purple', 'orange')
+let color = d3
+	.scaleLinear()
+	.domain([0, dataArray.length / 4])
+	.range([0, 1]);
+let colorInt = d3.interpolateHclLong("purple", "orange");
 //let color = d3.scaleLinear().domain([0, dataArray.length/4]).range(['blue', 'red']);
 
 let canvas = d3
@@ -32,77 +33,73 @@ let canvas = d3
 	.attr("height", height);
 let ctx = canvas.node().getContext("2d");
 
-let ARRAY=[];
-
-//console.log(c(0.5));
-//console.log(c(color(600)));
+let ARRAY = [];
 
 function draw() {
 	requestAnimationFrame(draw);
 	analyser.getByteFrequencyData(dataArray);
 	ARRAY.push(highest(dataArray));
-	ctx.fillStyle = 'black';
-	ctx.strokStyle= 'black';
-	ctx.fillRect(0,0,width,height);
-	for(let i=0;i<dataArray.length; i++){
+	ctx.fillStyle = "black";
+	ctx.strokStyle = "black";
+	ctx.fillRect(0, 0, width, height);
+	for (let i = 0; i < dataArray.length; i++) {
 		let c = posArray[i];
 		//ctx.strokeStyle=d3.interpolateSpectral(color(i));
-		ctx.strokeStyle=colorInt(color(i));
+		ctx.strokeStyle = colorInt(color(i));
 		ctx.beginPath();
 		ctx.arc(c.x, c.y, dataArray[i], 0, tau);
 		ctx.stroke();
 	}
-};
+}
 
-function highest(arr){
+function highest(arr) {
 	let res;
-	for(let i=0; i<arr.length; i++){
-		if(arr[i]>0)
-			res=i
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > 0) res = i;
 	}
 	return res;
 }
 
-function high(arr){
+function high(arr) {
 	let res;
-	for(let i=0; i<arr.length; i++){
-		if(arr[i]>res&&typeof arr[i]=='number')
-			res=arr[i]
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > res && typeof arr[i] == "number") res = arr[i];
 	}
 	return res;
 }
 
 const btn = document.getElementById("play");
 btn.onclick = btnClick;
-window.onkeypress = spaceBar
+window.onkeypress = spaceBar;
 
-function spaceBar(e){
-	if(e.code=='Space')
-		btnClick();
+function spaceBar(e) {
+	if (e.code == "Space") btnClick();
 }
 
 function btnClick() {
 	audioEl.paused ? play() : pause();
-};
-
-function play(){
-		audioEl.play();
-		d3.select(btn).classed('opaque', true);
-		draw();
-};
-
-function pause(){
-	audioEl.pause();
-	console.log(high(ARRAY));
-	d3.select(btn).classed('opaque', false);
 }
 
-function getPositions(l){
-	let res=[];
-	for(let i=0; i<l; i++){
+function play() {
+	audioCtx.resume().then(() => {
+		audioEl.play();
+		d3.select(btn).classed("opaque", true);
+		draw();
+	});
+}
+
+function pause() {
+	audioEl.pause();
+	console.log(high(ARRAY));
+	d3.select(btn).classed("opaque", false);
+}
+
+function getPositions(l) {
+	let res = [];
+	for (let i = 0; i < l; i++) {
 		const x = d3.randomUniform(0, width)();
 		const y = d3.randomUniform(0, height)();
-		res.push({x: x, y: y})
+		res.push({ x: x, y: y });
 	}
 	return res;
 }
